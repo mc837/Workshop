433d1c91ecc585d1c768e25fb217a43e8c40485a
Matt Clarkson <Matt.clarkson@comparethemarket.com>
2015-01-13 20:49:15 +0000

    Day 2

---
+++ Workshop/Probability (day2)/Probability.cs
@@ -0,0 +1,33 @@
+﻿namespace Workshop
+{
+    public class Probability
+    {
+        private readonly double _odds;
+
+        public Probability(double odds)
+        {
+            _odds = odds;
+        }
+        public Probability InversedProbability
+        {
+            get { return new Probability(1 - _odds); }
+        }
+
+        public Probability And(Probability probability)
+        {
+            return new Probability(probability._odds * _odds);
+        }
+
+        public Probability Or(Probability probability)
+        {
+            return InversedProbability.And(probability.InversedProbability).InversedProbability;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != typeof(Probability)) return false;
+            var probability = obj as Probability;
+            return _odds.Equals(probability._odds);
+        }
+    }
+}
---
+++ WorkshopTests/ProbabilityTests (day2)/ProbabilityTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using Workshop;
+
+namespace WorkshopTests
+{
+    public class ProbabilityTests
+    {
+        [Test]
+        public void Should_CompareIfTwoProbabilitiesAreSame()
+        {
+            var probability1 = new Probability(0.5);
+            var probability2 = new Probability(0.5);
+
+            Assert.That(probability1, Is.EqualTo(probability2));
+        }
+
+        [Test]
+        public void Should_StateInverseProbability_Of_AStatedProbability()
+        {
+            var probability = new Probability(0.25);
+            var inverseProbability = probability.InversedProbability;
+            var expectedInverseProbability = new Probability(0.75);
+
+            Assert.True(inverseProbability.Equals(expectedInverseProbability));
+        }
+
+        [Test]
+        public void Should_CalculateCombinedProbabilityForTwoOdds()
+        {
+            var expectedProbability = new Probability(0.25);
+
+            var probability1 = new Probability(0.5);
+            var probability2 = new Probability(0.5);
+
+            var combinedProbability = probability1.And(probability2);
+
+            Assert.True(combinedProbability.Equals(expectedProbability));
+        }
+
+
+        [Test]
+        public void Should_CalculateProbabilityForEitherOutcomeToOccur_WhenTwoProbabilitiesArePossible()
+        {
+            var expectedProbability = new Probability(0.55);
+            var probability1 = new Probability(0.4);
+            var probability2 = new Probability(0.25); ;
+            var resultingProbability = probability1.Or(probability2);
+            Assert.That(resultingProbability.Equals(expectedProbability));
+        }
+    }
+}
+
