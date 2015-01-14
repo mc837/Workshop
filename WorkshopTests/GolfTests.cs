095975e8dfeee0c2f12c70c683cc387e21c95339
Matt Clarkson <Matt.clarkson@comparethemarket.com>
2015-01-14 15:22:42 +0000

    Day 3 working for seperating groups of 4 and making groups of 4

--- WorkshopTests/GolfTests.cs
+++ WorkshopTests/GolfTests.cs
@@ -1,65 +1,80 @@
-﻿using System.Runtime.InteropServices;
-using System.Threading;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace WorkshopTests
 {
     class GolfTests
     {
-        [TestCase(new[] { 4 }, 1)]
-        [TestCase(new[] { 4, 3, 1 }, 2)]
+        //[TestCase(new[] { 4 }, 1)]
+        [TestCase(new[] { 4, 3 }, 2)]
         [TestCase(new[] { 4, 3, 4 }, 3)]
-        public void Should_CountTheAmountOfAllowedGroups_From_Que(int [] que, int count)
+        [TestCase(new[] { 4, 1, 4 }, 3)]
+        [TestCase(new[] { 4, 1, 4, 2 }, 4)]
+        [TestCase(new[] { 4, 1, 2, 2 }, 3)]
+        [TestCase(new[] { 1, 1, 1, 1 }, 1)]
+        [TestCase(new[] { 1, 2, 3, 4, 4, 2, 2, 1, 1 }, 6)]
+        public void Should_CountTheAmountOfAllowedGroups_From_Que(int[] que, int count)
         {
-            var organiser = new PlayerOrganiser(que);
+            var organiser = new Que(que);
             var result = organiser.Organise();
             Assert.That(result.Count, Is.EqualTo(count));
         }
     }
 
-    internal class PlayerOrganiser
+    internal class Que
     {
         private readonly int[] _que;
+        private PlayingGroup _playingGroup;
+        private List<PlayingGroup> playingGroups = new List<PlayingGroup>();
 
-        public PlayerOrganiser(int[] que)
+        public Que(int[] que)
         {
             _que = que;
         }
 
-        public Que Organise()
+        public List<PlayingGroup> Organise()
         {
-            var count = 0;
             for (int quePosition = 0; quePosition < _que.Length; quePosition++)
             {
                 var group = _que[quePosition];
-                switch (group)
-                {
-                    case 4:
-                        count ++;
-                        break;
-                    case 3:
-                        if (_que[(quePosition + 1)] == 1)
-                        {
-                            count++;
-                            quePosition++;
-                        }
-                        else
-                        {
-                            count++;
-                        }
-                        break;
-                }
-            }
 
-            return new Que
-            {
-                Count = count
-            };
+                if (group == 4)
+                {
+                    CreateNewGroup(group);
+                }
+                else
+                {
+                    var pos = quePosition;
+                    while ((pos + 1) < _que.Length)
+                    {
+                        while ((pos + 1) < _que.Length && group + _que[(pos + 1)] <= 4)
+                        {
+                            group += _que[(quePosition + 1)];
+                            quePosition ++;
+                            pos++;
+                        }
+                        break;
+                    }
+                    CreateNewGroup(group);
+                }
+
+            } return playingGroups;
+        }
+
+        private void CreateNewGroup(int groupNumber)
+        {
+            _playingGroup = new PlayingGroup { Members = groupNumber };
+            AddGroupToList(_playingGroup);
+        }
+
+        private void AddGroupToList(PlayingGroup group)
+        {
+            playingGroups.Add(_playingGroup);
         }
     }
 
-    internal class Que  
+    internal class PlayingGroup
     {
-        public int Count { get; set; }
+        public int Members { get; set; }
     }
 }
